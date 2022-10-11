7244318cadc098d57cd7e350a74c71673508fd42
Adam Cooper <adamcooper@uchicago.edu>
2022-10-11 10:52:47 -0500

    Separate cert loading into two steps

--- docker/letsencrypt/letsencrypt-certbot/haproxy-reload.sh
+++ docker/letsencrypt/letsencrypt-certbot/haproxy-reload.sh
@@ -7,8 +7,10 @@ if [[ -d "$le_base" ]]; then
     domains=$(find $le_base -mindepth 1 -type d -exec basename {} \;)
     for domain in $domains; do
         cert_path="/etc/haproxy/certs.d/$domain.pem"
+        # Get the full text of the certificate, deleting any blank lines (OpenSSL doesn't like those)
+        full_cert=$(cat $le_base/$domain/fullchain.pem $le_base/$domain/privkey.pem | sed '/^[[:blank:]]*$/ d')
         # Start a transaction to update the certificate
-        echo -e "set ssl cert $cert_path <<\n$(cat $le_base/$domain/fullchain.pem $le_base/$domain/privkey.pem | sed '/^[[:blank:]]*$/ d')\n" | socat /var/lib/kolla/haproxy/haproxy.sock -
+        echo -e "set ssl cert $cert_path <<\n$full_cert\n" | socat /var/lib/kolla/haproxy/haproxy.sock -
         # Commit the transaction
         echo "commit ssl cert $cert_path" | socat /var/lib/kolla/haproxy/haproxy.sock -
     done
